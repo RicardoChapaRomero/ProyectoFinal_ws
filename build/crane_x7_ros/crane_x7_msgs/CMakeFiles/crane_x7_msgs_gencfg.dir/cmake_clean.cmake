67ae8b964ca662711738334ecaf6c294e20e4584
RicardoChapaRomero <ricardochaparomero@gmail.com>
2022-05-29 21:50:49 -0500

    Init proyect withg ARMX7 robot

---
+++ build/crane_x7_ros/crane_x7_msgs/CMakeFiles/crane_x7_msgs_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/home/ricardochapa/Tec/Vision/ProyectoFinal_ws/devel/include/crane_x7_msgs/ServoParameterConfig.h"
+  "/home/ricardochapa/Tec/Vision/ProyectoFinal_ws/devel/lib/python3/dist-packages/crane_x7_msgs/cfg/ServoParameterConfig.py"
+  "/home/ricardochapa/Tec/Vision/ProyectoFinal_ws/devel/share/crane_x7_msgs/docs/ServoParameterConfig-usage.dox"
+  "/home/ricardochapa/Tec/Vision/ProyectoFinal_ws/devel/share/crane_x7_msgs/docs/ServoParameterConfig.dox"
+  "/home/ricardochapa/Tec/Vision/ProyectoFinal_ws/devel/share/crane_x7_msgs/docs/ServoParameterConfig.wikidoc"
+  "CMakeFiles/crane_x7_msgs_gencfg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/crane_x7_msgs_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
